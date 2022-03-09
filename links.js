7d62b49e27eb675b1c23e6a137f40cc2e75c0ece
CJ LG <58236313+cquejuan@users.noreply.github.com>
2022-03-08 21:02:55 -0900

    Add files via upload

---
+++ links.js
@@ -0,0 +1,19 @@
+let link = "https://eielson.net/"
+if (location.host !== link) {
+    //alert("Warning! This site is not authorized to use this script! You will now be redirected to the main host website.")
+    //location.replace(link)
+}
+let links = [
+    {
+        link: "https://www.alaskacommunications.com/Residential/Products/InternetNow",
+        title: "Internet Now",
+        header: "WiFi for the Dorms",
+        description: "Internet Now, Unlimited Internet by Alaska Communications for Eielson dorm residents. Starting at $60/month."
+    },
+    {
+        link: "https://gci.com/",
+        title: "GCI Internet",
+        header: "WiFi for the Dorms",
+        description: "GCI internet for the Dorms. Unlimited, starting at $80/month for 250 GB high speed data."
+    }
+]
